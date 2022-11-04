136ddaea52572d00897788f0ead2213f9510af8e
Junior <junior.zytkowski@gmail.com>
2022-11-04 20:23:19 -0300

    Complemented ServerEnvironmentVariables with new variable

--- src/main/kotlin/io/pipeliner/infrastructure/environment/ServerEnvironmentVariables.kt
+++ src/main/kotlin/io/pipeliner/infrastructure/environment/ServerEnvironmentVariables.kt
@@ -3,11 +3,15 @@ package io.pipeliner.infrastructure.environment
 object ServerEnvironmentVariables {
 
     val serverPort = System.getenv(Keys.SERVER_PORT)?.toIntOrNull() ?: Defaults.SERVER_PORT
+    val serviceKey = System.getenv(Keys.SERVICE_KEY) ?: Defaults.SERVICE_KEY
 
     private object Keys {
         const val SERVER_PORT = "SERVER_PORT"
+        const val SERVICE_KEY = "SERVICE_KEY"
     }
+
     private object Defaults {
         const val SERVER_PORT = 7070
+        const val SERVICE_KEY = "secret"
     }
 }
